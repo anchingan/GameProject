30b7b27c611a14873be4b7d5ce858fce08c46d4e
chingan <chingan@192.168.11.121>
2017-09-06 17:06:35 +0800

    Create repository.

--- 012_GameProject/src/gameProject/Ball.java
+++ 012_GameProject/src/gameProject/Ball.java
@@ -1,5 +1,19 @@
 package gameProject;
 
 public class Ball {
+	private double x, y;
+	
+	public Ball() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
 
 }
--- 012_GameProject/src/gameProject/TestGame.java
+++ 012_GameProject/src/gameProject/TestGame.java
@@ -3,7 +3,7 @@ package gameProject;
 public class TestGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
